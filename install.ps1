06bc9b84032812dd44ebfb4cfb8f37ee88b2c460
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2016-09-14 14:04:55 +0900

    git-masterをフォーク版から取得するよう変更

--- install.ps1
+++ install.ps1
@@ -22,7 +22,7 @@ cd $BinFolder
 Invoke-WebRequest https://github.com/git/git/raw/master/contrib/diff-highlight/diff-highlight -UseBasicParsing -OutFile diff-highlight
 Invoke-WebRequest https://gist.githubusercontent.com/masaru-b-cl/5b461ffd4728ab9b61cb/raw/bbd12fcd6928ef31f5ab3d7d7dc2b230f95202f8/git-feature -UseBasicParsing -OutFile git-feature
 Invoke-WebRequest https://gist.githubusercontent.com/masaru-b-cl/7038197/raw/fd9392748d0d5fba19af0122e845d04b6cfe2e9c/git-now -UseBasicParsing -OutFile git-now
-Invoke-WebRequest https://gist.githubusercontent.com/mzp/1131618/raw/900b1ed139f7ec3ded026eb09e97816d6f446b99/gistfile1.txt -UseBasicParsing -OutFile git-master
+Invoke-WebRequest https://gist.githubusercontent.com/masaru-b-cl/3753414/raw/89d20540d91a017874386b9a3e4d9f0169360c1a/git-master -UseBasicParsing -OutFile git-master
 Invoke-WebRequest https://gist.githubusercontent.com/masaru-b-cl/5ada3ccd4ad4627032ca/raw/b7217c3080d95bef1c4729019b34fe9efe5db70e/git-archive-diff -UseBasicParsing -OutFile git-archive-diff
 
 # prepare Git folder
@@ -51,7 +51,9 @@ git clone -b windows https://github.com/masaru-b-cl/dotfiles.git
 # install MyricaM
 $MyricaPath = $env:TEMP + "\MyricaM.TTC"
 Invoke-WebRequest https://github.com/tomokuni/Myrica/raw/master/product/MyricaM.TTC -UseBasicParsing -OutFile $MyricaPath
-$FONTS = 0x14$objShell = New-Object -ComObject Shell.Application$objFolder = $objShell.Namespace($FONTS)
+$FONTS = 0x14
+$objShell = New-Object -ComObject Shell.Application
+$objFolder = $objShell.Namespace($FONTS)
 $objFolder.CopyHere($MyricaPath)
 
 Set-Location $CurrentDir
